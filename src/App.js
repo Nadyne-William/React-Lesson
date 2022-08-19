1fc9f1f39078a79f6f239e949266b9e447b5be4b
Nadyne-William <frankekong33@gmail.com>
2022-08-19 15:23:03 +0100

    Created project for react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+ import { Fragment } from "react"
+
+ import "./stylesheet/style.css"
+
+function App(){
+ 
+  return(
+
+    <>
+     <h1>Hello</h1>
+
+    <h2>Hello</h2>
+    
+    <h2>Hello</h2>
+    </>
+
+  )
+}
+
+export default App
